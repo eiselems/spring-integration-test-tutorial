1cb3b8c6fd14c21b4b07aa557b8cc759ada8569d
eiselems <marcus.eisele@gmail.com>
2019-05-11 01:47:18 +0200

    INIT from start.spring.io

---
+++ src/main/java/com/programmerfriend/tutorial/integrationtest/IntegrationtestApplication.java
@@ -0,0 +1,13 @@
+package com.programmerfriend.tutorial.integrationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programmerfriend/tutorial/integrationtest/IntegrationtestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.programmerfriend.tutorial.integrationtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntegrationtestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
