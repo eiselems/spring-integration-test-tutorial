c2ac6e51df9429a0d337496a5a598ed4f0241205
eiselems <marcus.eisele@gmail.com>
2019-05-13 22:10:35 +0200

    Add working postgres container for test

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # spring-integration-test-tutorial
+
+This is the code repository for the tutorial at: http://localhost:4000/spring-boot-integration-testing-done-right/
+
+
+It is a showcase for leveraging the power of Docker, Testcontainers and RestAssured to properly Integration Test a Spring Boot 2 Application.
+
+As an example it is running an IntegrationTest which is bootstrapping a PostgreSQL Docker Container.
--- src/test/java/com/programmerfriend/tutorial/integrationtest/IntegrationtestApplicationTests.java
+++ src/test/java/com/programmerfriend/tutorial/integrationtest/IntegrationtestApplicationTests.java
@@ -1,16 +1,74 @@
 package com.programmerfriend.tutorial.integrationtest;
 
+import org.assertj.core.api.Assertions;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(initializers = {IntegrationtestApplicationTests.Initializer.class})
 public class IntegrationtestApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @ClassRule
+    public static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres")
+            .withDatabaseName("postgres")
+            .withUsername("postgres")
+            .withPassword("docker");
 
+    @Value("http://localhost:${local.server.port}")
+    String baseUrl;
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void testWriteToDb_afterBoot_shouldHaveEntries() {
+        List<Employee> all = employeeRepository.findAll();
+        Assertions.assertThat(all.size()).isEqualTo(6);
+        Assertions.assertThat(all.get(0).getFirstName()).isEqualTo("First");
+        Assertions.assertThat(all.get(0).getLastName()).isEqualTo("Last");
+    }
+
+    @Test
+    public void testGet_returns_200_with_expected_employees() {
+
+        when().
+            get(baseUrl + "/employees").
+        then()
+            .statusCode(200)
+            .body("size()", is(6))
+            .body("[0].firstName", equalTo("First"))
+            .body("[0].lastName", equalTo("Last"));
+    }
+
+    static class Initializer
+            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + postgres.getJdbcUrl(),
+                    "spring.datasource.username=" + postgres.getUsername(),
+                    "spring.datasource.password=" + postgres.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
 }
